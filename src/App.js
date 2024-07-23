d1f53c68629bf420b1b629be0c5d2a6d7242c8b3
shivanksharma4 <shivanksharma013@gmail.com>
2024-07-24 03:23:32 +0530

    Initial configuration and NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overflow-x-hidden text-neutral-300 antialiased
+    selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="flixed top-0 -z-10 h-full w-full"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="container mx-auto px-8">
+      <NavBar />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import logo from '../assets/kevinRushLogo.png'
+const NavBar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+          <img className='mx-2 w-10' src={logo} alt="logo"/>
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin/>
+        <FaGithub/>
+        <FaInstagram/>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
