ee2e66331e199e34fc7c16dcf1ee4485bc8786ba
shivanksharma4 <shivanksharma013@gmail.com>
2024-07-27 03:26:31 +0530

    Added Profile Section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container mx-auto px-8">
       <NavBar />
+      <Hero/>
       </div>
     </div>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { HERO_CONTENT } from "../constants/data";
+import ProPic from "../assets/kevinRushProfile.png"
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1
+              className="pb-16 text-4xl font-thin tracking-tight lg:mt-16
+                    lg:text-6xl"
+            >
+              Shivank Sharma
+            </h1>
+            <span
+              className="bg-gradient-to-r from-pink-300 via-slate-500
+                    to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+            >
+              Full Stack Developer
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {HERO_CONTENT}
+            </p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-10">
+            <div className="flex justify-center">
+                <img src={ProPic} alt="SS"/>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
