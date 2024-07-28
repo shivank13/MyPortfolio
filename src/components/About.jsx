692030fe19fc7b7cb17867917e13a71c97c141b4
shivanksharma4 <shivanksharma013@gmail.com>
2024-07-28 18:06:23 +0530

    Added motion in About and technologies section.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import aboutImg from "../assets/about.jpg"
 import { ABOUT_TEXT } from '../constants/data'
+import { motion } from 'framer-motion'
 
 const About = () => {
   return (
@@ -10,17 +11,25 @@ const About = () => {
             <span className='text-neutral-500'> Me</span>
         </h1>
         <div className='flex flex-wrap'>
-            <div className='w-full lg:w-1/2 lg:p-8'>
+            <motion.div 
+            initial={{x:-100, opacity:0}}
+            whileInView={{x:0, opacity:1}}
+            transition={{duration:0.5}}
+            className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex items-center justify-center'>
                     <img className='rounded-2xl' src={aboutImg} alt='about'/>
                 </div>
-            </div>
-            <div className='w-full lg:w-1/2'>
+            </motion.div>
+            <motion.div 
+            initial={{x:100, opacity:0}}
+            whileInView={{x:0, opacity:1}}
+            transition={{duration:0.5}}
+            className='w-full lg:w-1/2'>
                 <div className='flex justify-center lg:justify-start'>
                     <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
                 </div>
 
-            </div>
+            </motion.div>
         </div>
     </div>
   )
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,33 +1,102 @@
-import React from 'react'
-import { RiReactjsLine } from 'react-icons/ri'
-import { SiJavascript, SiPython, SiDjango, SiTailwindcss, SiMongodb } from 'react-icons/si';
+import { animate } from "framer-motion";
+import React from "react";
+import { RiReactjsLine } from "react-icons/ri";
+import {
+  SiJavascript,
+  SiPython,
+  SiDjango,
+  SiTailwindcss,
+  SiMongodb,
+} from "react-icons/si";
+import { motion } from "framer-motion";
+
+const IconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
-    <div className='border-b border-neutral-800 pb-24'>
-        <h1 className='my-20 text-center text-4xl'>Technologies</h1>
-        <div className='flex flex-wrap items-center justify-center gap-4'>
-        <div title="React" className='rounded-2xl border-4 border-neutral-800 p-4'>
-            <RiReactjsLine className='text-7xl text-[#61DAFB]' />  {/* React Color */}
-        </div>
-        <div title="JavaScript" className='rounded-2xl border-4 border-neutral-800 p-4'>
-            <SiJavascript className='text-7xl text-[#F7DF1E]' />  {/* JavaScript Color */}
-        </div>
-        <div title="TailwindCSS" className='rounded-2xl border-4 border-neutral-800 p-4'>
-            <SiTailwindcss className='text-7xl text-[#06B6D4]' />  {/* TailwindCSS Color */}
-        </div>
-        <div title="Python" className='rounded-2xl border-4 border-neutral-800 p-4'>
-            <SiPython className='text-7xl text-[#3776AB]' />  {/* Python Color */}
-        </div>
-        <div title="Django" className='rounded-2xl border-4 border-neutral-800 p-4'>
-            <SiDjango className='text-7xl text-[#092E20]' />  {/* Django Color */}
-        </div>
-        <div title="MongoDB" className='rounded-2xl border-4 border-neutral-800 p-4'>
-            <SiMongodb className='text-7xl text-[#47A248]' />  {/* MongoDB Color */}
-        </div>
-        </div>
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h1 
+      initial={{y:-100, opacity:0}}
+      whileInView={{y:0, opacity:1}}
+      transition={{duration:1.5}}
+      className="my-20 text-center text-4xl">Technologies</motion.h1>
+      <motion.div 
+      initial={{x:-100, opacity:0}}
+      whileInView={{x:0, opacity:1}}
+      transition={{duration:1.5}}
+      className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div
+          variants={IconVariants(2.5)} 
+          initial="initial"
+          animate="animate"
+          title="React"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-[#61DAFB]" />{" "}
+          {/* React Color */}
+        </motion.div>
+        <motion.div
+          variants={IconVariants(5)} 
+          initial="initial"
+          animate="animate"
+          title="JavaScript"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiJavascript className="text-7xl text-[#F7DF1E]" />{" "}
+          {/* JavaScript Color */}
+        </motion.div>
+        <motion.div
+        variants={IconVariants(4)} 
+        initial="initial"
+        animate="animate"
+          title="TailwindCSS"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiTailwindcss className="text-7xl text-[#06B6D4]" />{" "}
+          {/* TailwindCSS Color */}
+        </motion.div>
+        <motion.div
+        variants={IconVariants(3)} 
+        initial="initial"
+        animate="animate"
+          title="Python"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiPython className="text-7xl text-[#3776AB]" /> {/* Python Color */}
+        </motion.div>
+        <motion.div
+        variants={IconVariants(5.5)} 
+        initial="initial"
+        animate="animate"
+          title="Django"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiDjango className="text-7xl text-[#092E20]" /> {/* Django Color */}
+        </motion.div>
+        <motion.div
+        variants={IconVariants(4.5)} 
+        initial="initial"
+        animate="animate"
+          title="MongoDB"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-[#47A248]" />{" "}
+          {/* MongoDB Color */}
+        </motion.div>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Technologies
+export default Technologies;
