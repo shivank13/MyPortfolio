f96b1b652d9dae8df51e53f7b62f32cf747d1540
shivanksharma4 <shivanksharma013@gmail.com>
2024-07-30 01:48:44 +0530

    Added project and experience data and github links to projects

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,71 +1,55 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2.5 years of hands-on experience, I have honed my skills in front-end technologies like React and JavaScript, as well as back-end technologies like Python, Django and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "Aug 2022 - Present",
+    role: "Software Engineer - I",
+    company: "Hashedin By Deloitte",
+    description: `Developed innovative software solutions using React and Django, focusing on real-time data communication and enhancing workflow efficiency. Led development of user-centric interfaces with React, optimizing performance and satisfaction. Created and integrated RESTful APIs with Django,
+     improving data management and team collaboration. Implemented key functionalities and ensured high-quality, reliable applications through rigorous testing. Collaborated with stakeholders for Project timelines and Sprint planning.`,
+    technologies: ["Javascript", "React", "Python", "Django", "mongoDB"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "Jan 2022 - Aug 2022",
+    role: "Software Engineer Intern",
+    company: "Hashedin By Deloitte",
+    description: `During my 7 months internship, I was exposed to several tools and
+    technologies like HTML, CSS, Typescript, React, Java, springboot,
+    Python, Django and Angular and Worked as a Full stack dev(React and Django) on a product.`,
+    technologies: ["HTML", "CSS", "React", "Python", "Django"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "NETFLIX-GPT",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "A netflix clone website having GPT search feature and user authentication.",
+    technologies: ["React","Redux","TailwindCSS","Firebase"],
+    githublink:"https://github.com/shivank13/netflix-gpt"
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      "A personal portfolio website showcasing experience, projects, skills, and contact information.",
+    technologies: ["React", "TailwindCSS"],
+    githublink:"https://github.com/shivank13/ShivankPortfolio",
   },
   {
-    title: "Blogging Platform",
-    image: project4,
+    title: "Food Ordering Website",
+    image: project2,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A food ordering website having features like Search, filter, Accordian and Add to cart.",
+    technologies: ["React","Redux","TailwindCSS"],
+    githublink:"https://github.com/shivank13/ReactProject",
   },
 ];
 
