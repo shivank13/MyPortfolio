421c50f1772db4671f4ca24996bd586ff0d6ed05
shivanksharma4 <shivanksharma013@gmail.com>
2024-07-30 02:45:52 +0530

    Added download cv functionality

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HERO_CONTENT } from "../constants/data";
 import ProPic from "../assets/kevinRushProfile.png";
 import { motion } from "framer-motion";
+import { HiDownload } from "react-icons/hi";
 
 const container = (delay) => ({
   hidden: { x: -100, opacity: 0 },
@@ -28,30 +29,49 @@ const Hero = () => {
               Shivank Sharma
             </motion.h1>
             <motion.span
-             variants={container(0.5)}
-             initial="hidden"
-             animate="visible"
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
               className="text-center bg-gradient-to-r from-pink-300 via-slate-500
                     to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
             >
               Full Stack Developer
             </motion.span>
+            <motion.div
+            variants={container(0.5)}
+            initial="hidden"
+            animate="visible">
+            <a
+              href="/ShivankSharma.pdf"
+              download="ShivankSharma_CV.pdf"
+              className="flex items-center"
+            >
+              <HiDownload className="mr-2 text-xl" />{" "}
+              <span className="text-xl">Download CV</span>
+            </a>
+            </motion.div>
             <motion.p
-             variants={container(1)}
-             initial="hidden"
-             animate="visible" 
-             className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
               {HERO_CONTENT}
             </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-10">
           <div className="flex justify-center">
-            <motion.img 
-            initial={{x:100, opacity:0}}
-            animate={{x:0,opacity:1,
-            transition:{duration:1, delay:1}}}
-            src={ProPic} alt="SS" />
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{
+                x: 0,
+                opacity: 1,
+                transition: { duration: 1, delay: 1 },
+              }}
+              src={ProPic}
+              alt="SS"
+            />
           </div>
         </div>
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { FaLinkedin } from 'react-icons/fa'
-import { FaGithub } from 'react-icons/fa'
-import { FaInstagram } from 'react-icons/fa'
-import logo from '../assets/kevinRushLogo.png'
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import logo from "../assets/kevinRushLogo.png";
+import { HiDownload } from "react-icons/hi";
+
 const NavBar = () => {
   return (
-    <nav className='mb-16 flex items-center justify-between py-6'>
-      <div className='flex flex-shrink-0 items-center'>
-          <img className='mx-2 w-10' src={logo} alt="logo"/>
+    <nav className="mb-16 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
-      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <FaLinkedin/>
-        <FaGithub/>
-        <FaInstagram/>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/shivank-sharma13">
+          <FaLinkedin />
+        </a>
+        <a href="https://www.github.com/shivank13">
+          <FaGithub />
+        </a>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
